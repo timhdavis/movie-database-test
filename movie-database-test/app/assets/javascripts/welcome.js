cfb57ed292519bc8878ce51b1b8f8da55adb7a96
Tim Davis <timhdavis@yahoo.com>
2018-12-07 16:26:06 -0800

    got current movies from TMDb API
    -added list of movie titles on home page.

--- movie-database-test/app/assets/javascripts/welcome.js
+++ movie-database-test/app/assets/javascripts/welcome.js
@@ -6,18 +6,37 @@
 
 $(function(){
 
-    var $users_ul = $('#users');
+    //var $users_ul = $('#users');
+
+    // TEST:
+    // $.ajax({
+    //     type: 'GET',
+    //     url: 'https://jsonplaceholder.typicode.com/users',
+    //     success: function(users) {
+    //         console.log('success', users);
+
+    //         $.each(users, function(i, user) {
+    //             console.log("i=" + i + ", user.id=" + user.id + ", user.name=" + user.name);
+
+    //             $users_ul.append('<li>id: ' + user.id + ', name: ' + user.name + '</li>');
+    //         });
+    //     }
+    // });
+
+    var $movies_ul = $('#movies');
 
     $.ajax({
         type: 'GET',
-        url: 'https://jsonplaceholder.typicode.com/users',
-        success: function(users) {
-            console.log('success', users);
+        url: 'https://api.themoviedb.org/3/movie/now_playing?page=1&api_key=3ef6eb2432d3b79bfdb2b5bd0702e803',
+        success: function(data) {
+            console.log('success', data);
+            
+            var movies = data.results;
 
-            $.each(users, function(i, user) {
-                console.log("i=" + i + ", user.id=" + user.id + ", user.name=" + user.name);
+            $.each(movies, function(i, movie) {
+                console.log("i=" + i + ", movie.title= " + movie.title);
 
-                $users_ul.append('<li>id: ' + user.id + ', name: ' + user.name + '</li>');
+                $movies_ul.append('<li>title: ' + movie.title + '</li>');
             });
         }
     });
