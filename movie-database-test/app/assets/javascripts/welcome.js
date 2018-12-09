4822d83bc8059f27d8217bdbba5af6231ed55129
Tim Davis <timhdavis@yahoo.com>
2018-12-08 16:42:07 -0800

    added styles for 2 column sections

--- movie-database-test/app/assets/javascripts/welcome.js
+++ movie-database-test/app/assets/javascripts/welcome.js
@@ -7,21 +7,21 @@
 // jQeury:
 $(function(){
 
-    var $movies_ul = $('#movies');
+    // var $movies_ul = $('#movies_ul');
 
-    $.ajax({
-        type: 'GET',
-        url: 'https://api.themoviedb.org/3/movie/now_playing?page=1&api_key=3ef6eb2432d3b79bfdb2b5bd0702e803',
-        success: function(data) {
-            console.log('success', data);
+    // $.ajax({
+    //     type: 'GET',
+    //     url: 'https://api.themoviedb.org/3/movie/now_playing?page=1&api_key=3ef6eb2432d3b79bfdb2b5bd0702e803',
+    //     success: function(data) {
+    //         console.log('success', data);
             
-            var movies = data.results;
+    //         var movies = data.results;
 
-            $.each(movies, function(i, movie) {
-                // console.log("i=" + i + ", movie.title= " + movie.title);
+    //         $.each(movies, function(i, movie) {
+    //             // console.log("i=" + i + ", movie.title= " + movie.title);
 
-                $movies_ul.append('<li>title: ' + movie.title + '</li>');
-            });
-        }
-    });
+    //             $movies_ul.append('<li>title: ' + movie.title + '</li>');
+    //         });
+    //     }
+    // });
 });
