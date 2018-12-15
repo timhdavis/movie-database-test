85c73d5c6c78eb618fb212fc6e9a016ad8058593
Tim Davis <timhdavis@yahoo.com>
2018-12-15 11:24:36 -0800

    added review acceptance testing
    -added add review testing in browser using capybara, selenium, and chromedriver.
    --downloaded chromedriver using: "brew tap homebrew/cask" then "brew cask install chromedriver".
    -tests are run using Minitest:
    --run command "rails test" to run all tests.

---
+++ movie-database-test/test/integration/review_flow_test.rb
@@ -0,0 +1,117 @@
+require 'test_helper'
+
+class ReviewFlowTest < Capybara::Rails::TestCase
+  def setup
+    @review_1 = reviews(:valid_review_1)
+    @review_2 = reviews(:valid_review_2)
+  end
+
+  test 'review index' do
+    visit reviews_path
+
+    assert page.has_content?(@review_1.email)
+    assert page.has_content?(@review_1.email)
+  end
+
+  test 'writing a new review' do
+    movie = movies(:valid_movie_1)
+    visit movie_path(movie)
+
+    email = 'test_email_add_review_1@email.com'
+    comment = 'Test Comment'
+
+    fill_in 'Email', with: email
+    fill_in 'Comment',  with: comment
+
+    select_star(2)
+
+    click_on 'Add Review'
+
+    assert_current_path movie_path(movie)
+    assert page.has_content?(email)
+    assert page.has_content?(comment)
+  end
+
+  test 'writing a new review with min rating' do
+    movie = movies(:valid_movie_1)
+    visit movie_path(movie)
+
+    email = 'test_email_add_review_1@email.com'
+    comment = 'Test Comment'
+
+    fill_in 'Email', with: email
+    fill_in 'Comment',  with: comment
+
+    select_star(1)
+
+    click_on 'Add Review'
+
+    assert_current_path movie_path(movie)
+    assert page.has_content?(email)
+    assert page.has_content?(comment)
+  end
+
+  test 'writing a new review with max rating' do
+    movie = movies(:valid_movie_1)
+    visit movie_path(movie)
+
+    email = 'test_email_add_review_1@email.com'
+    comment = 'Test Comment'
+
+    fill_in 'Email', with: email
+    fill_in 'Comment',  with: comment
+
+    select_star(5)
+
+    click_on 'Add Review'
+
+    assert_current_path movie_path(movie)
+    assert page.has_content?(email)
+    assert page.has_content?(comment)
+  end
+
+  test 'writing a new review without rating' do
+    movie = movies(:valid_movie_1)
+    visit movie_path(movie)
+
+    email = 'test_email_add_review_2@email.com'
+    comment = 'Test Comment'
+
+    fill_in 'Email', with: email
+    fill_in 'Comment',  with: comment
+
+    # Without adding rating.
+
+    click_on 'Add Review'
+
+    assert_current_path movie_path(movie)
+    assert page.has_content?("Rating is not included in the list")
+  end
+
+  test 'writing a new review without email' do
+    movie = movies(:valid_movie_1)
+    visit movie_path(movie)
+
+    fill_in 'Comment',  with: 'Test Comment'
+
+    # Use jQuery to select Raty star rating in form:
+    select_star(3)
+
+    click_on 'Add Review'
+
+    assert_current_path movie_path(movie)
+    assert page.has_content?("Email can't be blank")
+  end
+
+  private
+
+  def select_star(star_rating)
+    # Use jQuery to select the Raty star rating in form:
+
+    within ("#rating-form") do
+      # Each Raty star image has alt={1..5 depending on star order}:
+      page.find(:css, "img[alt='#{star_rating.to_s}']").click
+    end
+  end
+
+end
