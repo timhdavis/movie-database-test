9be047c933a1c522681b78b5538966c232436180
Tim Davis <timhdavis@yahoo.com>
2018-12-15 16:13:33 -0800

    starting to try without raty stars
    -Heroku cannot load star images through Raty (because of how assets are stored and how Raty accesses these assets).

--- movie-database-test/app/controllers/application_controller.rb
+++ movie-database-test/app/controllers/application_controller.rb
@@ -2,11 +2,25 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_page_title
+  before_action :set_star_path
 
   private 
   
   def set_page_title
     @page = "home"
+  end
+
+  def set_star_path
+    # group :development, :test do
+    #   @star_path = "/assets/"
+    # end
+
+    # group :production do
+    #   @star_path = "public/assets/"
+    # end
+
+    @star_path = "/assets/"
+    
   end
 
 end
